f399354c29bc958ebebc42d9363a76e98d33bb56
mattiapomelli <mattiapomelli@gmail.com>
2021-01-18 20:58:04 +0100

    conditional popup screen

--- app/scripts/popup.ts
+++ app/scripts/popup.ts
@@ -1,14 +1,37 @@
 // Enable chromereload by uncommenting this line:
 import 'chromereload/devonly'
 
-const button = document.getElementById("download-button")
-button?.addEventListener("click", sendMessage)
 
-function sendMessage() {
-  chrome.tabs.query({currentWindow: true, active: true}, function(tabs) {
-    const message = {
-      type: "download"
-    }
-    chrome.runtime.sendMessage(message);
-  });
+const notesScreen = document.getElementById("notes-container")
+const downloadScreen = document.getElementById("download-container")
+
+const notesButton = document.getElementById("notes-button")
+notesButton?.addEventListener("click", startNotesSession)
+
+const downloadButton = document.getElementById("download-button")
+downloadButton?.addEventListener("click", startDownloaad)
+
+
+if(window.localStorage.getItem('status') == 'active') {
+  showScreen("download")
+}
+
+
+function startDownloaad() {
+  chrome.runtime.sendMessage({ type: "download"});
+}
+
+function startNotesSession() {
+  showScreen("download")
+  window.localStorage.setItem("status", "active");
+  chrome.runtime.sendMessage({ type: "new-notes" });
+}
+
+function showScreen(screen: "notes" | "download") {
+  if(notesScreen) {
+    notesScreen.style.display = screen === "notes" ? "block" : "none";
+  }
+  if(downloadScreen) {
+    downloadScreen.style.display = screen === "download" ? "block" : "none";
+  }
 }
