ada6d395b95aaf134c060d11834dfea0bc50a805
mattiapomelli <mattiapomelli@gmail.com>
2021-01-19 21:14:10 +0100

    html template with basic styling

---
+++ server/utils/template.js
@@ -0,0 +1,28 @@
+module.exports = getHTMLfromTemplate = (content) => {
+  return `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Document</title>
+      <style>
+        body {
+          margin: 0;
+          font-family: "Segoe UI", sans-serif;
+        }
+    
+        #container {
+          width: 90%;
+          max-width: 800px;
+          margin: 0 auto;
+          padding-top: 4rem;
+        }
+      </style>
+    </head>
+    <body>
+      <div id="container">
+        ${content}
+      </div>
+    </body>
+    </html>`
+}
