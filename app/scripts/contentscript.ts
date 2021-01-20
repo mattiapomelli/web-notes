b0839052733d747ed229ba84c3fc2b15ecabc47d
mattiapomelli <mattiapomelli@gmail.com>
2021-01-20 22:10:49 +0100

    notification when notes taken

--- app/scripts/contentscript.ts
+++ app/scripts/contentscript.ts
@@ -25,14 +25,14 @@ chrome.runtime.onMessage.addListener((message: MessageType) => {
 
 })
 
-function mouseUpHandler() {
+function mouseUpHandler(event: MouseEvent) {
   if(window.getSelection()) {
     const selection = window.getSelection() ?? ''
     const selectedText = selection.toString()
   
     if(selectedText.length > 0) {
-      console.log(`%c Text selected: ${selectedText}`, "color: blue")
-      console.log(getHTMLOfSelection())
+      // console.log(`%c Text selected: ${selectedText}`, "color: blue")
+      // console.log(getHTMLOfSelection())
   
       const message = {
         type: "notes",
@@ -41,6 +41,38 @@ function mouseUpHandler() {
       }
   
       chrome.runtime.sendMessage(message)
+      createNotification(event.clientX, event.pageY - 30)
     }
   }
+}
+
+// create a notification on the DOM telling that notes have been taken
+function createNotification(x: number, y: number) {
+  const notification = document.createElement("div")
+  notification.innerText = "Notes taken ✔️"
+
+  const styles = {
+    color: "black",
+    backgroundColor: "#eee",
+    borderRadius: "10px",
+    border: "1px solid #ccc",
+    position: "absolute",
+    padding: "5px 10px"
+  }
+
+  Object.assign(notification.style, styles)
+  document.body.appendChild(notification)
+  
+  notification.animate([
+    { opacity: '0' },
+    { opacity: '1' }
+  ], {
+    duration: 300
+  });
+
+  notification.style.top = `${y}px`
+  notification.style.left = `${x}px`;
+  window.setTimeout(() => {
+    notification.remove()
+  }, 2000)
 }
