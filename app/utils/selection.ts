255589f83415d64a35a68a94c3f65a276b61895f
mattiapomelli <mattiapomelli@gmail.com>
2021-01-21 21:31:14 +0100

    fixed problem on selection

--- app/utils/selection.ts
+++ app/utils/selection.ts
@@ -1,3 +1,5 @@
+const textTags = ["h1", "h2", "h3", "h4",  "h5", "h6", "p"]
+
 export default function getHTMLOfSelection() {
   
   if (window.getSelection) {
@@ -6,21 +8,29 @@ export default function getHTMLOfSelection() {
     if (selection && selection.rangeCount > 0) {
       const range = selection.getRangeAt(0);
 
-      let node = range.startContainer;
+      const startNode = range.startContainer;
       const endNode = range.endContainer;
     
       // Special case for a range that is contained within a single node
-      if (node == endNode) {
-        const tag = node.parentElement?.localName
-        return `<${tag}>${node.nodeValue}</${tag}>`
+      if (startNode == endNode) {
+        console.log("one node")
+        const tag = startNode.parentElement?.localName
+        return `<${tag}>${selection.toString()}</${tag}>`   // put only selection instead of whole value ?
+      }
+      
+      const clonedSelection = range.cloneContents();
+
+      const div = document.createElement('div');    
+      div.appendChild(clonedSelection);
+      const selectionHTML = div.innerHTML
+
+      const ancestor = range.commonAncestorContainer.nodeName.toLocaleLowerCase()
+
+      if(textTags.includes(ancestor)) {
+        return `<${ancestor}>${selectionHTML}</${ancestor}>`
       }
 
-      const clonedSelection = range.cloneContents();
-      const div = document.createElement('div');
-      
-      div.appendChild(clonedSelection);
-
-      return div.innerHTML
+      return selectionHTML
     }
     else {
       return '';
