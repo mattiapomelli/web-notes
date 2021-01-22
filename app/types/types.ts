546e1ec6c8e82f8d2f58fb707a228b7d75f7c477
mattiapomelli <mattiapomelli@gmail.com>
2021-01-22 13:25:13 +0100

    edited message type

--- app/types/types.ts
+++ app/types/types.ts
@@ -1,11 +1,7 @@
 type FormatType = "md" | "html" | "txt"
 
-interface MessageType {
-  type: "download" | "notes" | "new-session" | "reset-session",
-  format?: FormatType,
-  text?: string,
-  html?: string,
-  title?: string
+type MessageType = {
+  type: "new-session" | "reset-session",
 }
 
 type NotesType = {
