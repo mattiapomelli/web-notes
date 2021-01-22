f3ae3d1933e4101a0487d455ef76f6e0f5bfeb9d
mattiapomelli <mattiapomelli@gmail.com>
2021-01-22 16:38:27 +0100

    function to get notes content to download

--- api/functions/hello.js
+++ api/functions/hello.js
@@ -1,13 +1,38 @@
+const getHTMLfromTemplate = require('../utils/template')
 const sanitizeHtml = require('sanitize-html');
+const TurndownService = require('turndown')
+const beautifyHtml = require('js-beautify').html;
+
+const turndownService = new TurndownService({ headingStyle: 'atx' })
 
 exports.handler = async event => {
-  const subject = event.queryStringParameters.name || 'World'
-  const html = '<h1 class="something">Hey!!</h1>'
 
-  const cleanHTML = sanitizeHtml(html);
+  if(event.httpMethod === 'POST') {
+    const { data, format, contentOnly = false } = JSON.parse(event.body)
 
-  return {
-    statusCode: 200,
-    body: cleanHTML,
+    const cleanHTML = sanitizeHtml(data);
+
+    let result;
+
+    if(format === 'md') {
+      result = turndownService.turndown(cleanHTML)
+    } else {
+      if(contentOnly) {
+        result = beautifyHtml(cleanHTML)
+      } else {
+        result = beautifyHtml(getHTMLfromTemplate(cleanHTML))
+      }
+    }
+
+    return {
+      statusCode: 200,
+      body: result,
+    }
+
+  } else {
+    return {
+      statusCode: 405,
+      body: "Method not allowed",
+    }
   }
 }
