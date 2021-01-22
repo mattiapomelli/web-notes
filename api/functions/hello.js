89cdcb2a8fe19baecccbad616f8bf7b0cc64e19f
mattiapomelli <mattiapomelli@gmail.com>
2021-01-22 16:15:34 +0100

    functions config

--- api/functions/hello.js
+++ api/functions/hello.js
@@ -1,7 +1,13 @@
+const sanitizeHtml = require('sanitize-html');
+
 exports.handler = async event => {
   const subject = event.queryStringParameters.name || 'World'
+  const html = '<h1 class="something">Hey!!</h1>'
+
+  const cleanHTML = sanitizeHtml(html);
+
   return {
     statusCode: 200,
-    body: `Hello ${subject}!`,
+    body: cleanHTML,
   }
 }
